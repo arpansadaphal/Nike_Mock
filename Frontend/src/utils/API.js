dbbbbe049bb824492a0b89fbdbb7ec28cb0e6f69
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2025-02-02 17:02:16 +0530

    Update API.js

--- Frontend/src/utils/API.js
+++ Frontend/src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://nike-mock.onrender.com/api",
 });
 
 // export const fetchProducts = async () => {
