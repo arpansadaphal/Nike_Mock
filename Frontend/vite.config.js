c9a1e70a6307c0cb48758364922eb43a56c05830
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2025-02-03 14:53:46 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://nike-mock.onrender.com",
-     // "/api": "http://127.0.0.1:8000",
+     // "/api": "https://nike-mock.onrender.com",
+      "/api": "http://127.0.0.1:8000",
       
     },
   },
