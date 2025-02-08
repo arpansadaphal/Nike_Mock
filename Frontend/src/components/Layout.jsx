538bd57d821212470fa9f64cdc8f35c9d0e5b555
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2025-02-08 20:36:26 +0530

    Update Layout.jsx

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -7,8 +7,8 @@ const Layout = ({ children }) => {
   return (
     <div className="product-layout">
       <Nav className="fixed top-0 left-0 right-0 z-50  shadow-md" />
-      <div className="container mx-auto px-4 pt-24 ">{children}</div>
-    <div>  <br> <br> <br> <br>  </div>
+      <div className="container mx-auto px-4 pt-24 pb-8">{children}</div>
+   
       <div className="bg-black padding-x padding-t pt-5 pb-8">
                 <Footer />
               
