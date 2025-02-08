ff782c882c0a2e6238b2f0c0f0de22167f037637
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2025-02-08 19:24:46 +0530

    Update Layout.jsx

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
       <Nav className="fixed top-0 left-0 right-0 z-50  shadow-md" />
       <div className="container mx-auto px-4 pt-24 ">{children}</div>
       <div className="padding-x padding-t pt-5 pb-8">
-        <footer className="max-container pt-10 bg-gray-900">
+        <Footer />
 
       </div>
     </div>
