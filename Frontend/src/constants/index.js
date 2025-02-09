f794f34d96f0c213dd24a03fc0873ffe4285cd57
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2025-02-09 21:36:28 +0530

    Update index.js

--- Frontend/src/constants/index.js
+++ Frontend/src/constants/index.js
@@ -23,8 +23,8 @@ import {
 
 export const navLinks = [
   { href: "/", label: "Home" },
-  { href: "#about-us", label: "About Us" },
-  //{ href: "/products", label: "Products" },
+ // { href: "#about-us", label: "About Us" },
+  { href: "/products", label: "Products" },
   { href: "/cart", label: "Cart" },
   // { href: "/orders", label: "Orders" },
 ];
