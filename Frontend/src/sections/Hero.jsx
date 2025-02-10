6a96a22f5d91384022aad7624af849c5636c825a
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2025-02-10 15:31:15 +0530

    Update Hero.jsx

--- Frontend/src/sections/Hero.jsx
+++ Frontend/src/sections/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
           Our Summer collections
         </p>
 
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+        <h1 className=" relative mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 pl-5">
             New Arrival
           </span>
