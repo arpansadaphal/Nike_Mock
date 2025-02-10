d157ecfd4b58c2c2729fee3c7e65d383682a4689
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2025-02-10 20:27:42 +0530

    Update Hero.jsx

--- Frontend/src/sections/Hero.jsx
+++ Frontend/src/sections/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           Our Summer Collection
         </p>
 
-        <h1 className="mt-6 text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold text-gray-900 leading-tight">
+        <h1 className="mt-6 text-7xl md:text-8xl font-bold text-gray-900 leading-tight">
           <span className="relative z-10 pr-10 pl-2">New Arrival</span>
           <br />
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
@@ -37,7 +37,8 @@ const Hero = () => {
 
       {/* Right Content - Shoe Image & Selection */}
       <div className="relative flex-1 flex flex-col items-center xl:items-end xl:justify-center xl:w-1/2">
-        <div className="relative w-[450px] h-[450px] bg-white shadow-xl rounded-full flex items-center justify-center">
+        {/* Shoe Image - Adjusting size for different screens */}
+        <div className="relative w-[350px] md:w-[400px] xl:w-[450px] h-[350px] md:h-[400px] xl:h-[450px] bg-white shadow-xl rounded-full flex items-center justify-center">
           <img
             src={bigShoeImg}
             alt="shoe collection"
@@ -46,7 +47,7 @@ const Hero = () => {
         </div>
 
         {/* Shoe Selection */}
-        <div className="flex gap-6 mt-6">
+        <div className="flex gap-4 md:gap-6 mt-6 md:mt-8 xl:mt-10 max-sm:px-4 sm:px-6 md:px-8">
           {shoes.map((image, index) => (
             <ShoeCard
               key={index}
