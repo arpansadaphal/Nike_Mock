799bfbb09d90c286cac3efd0c0b8e4566d7a93b8
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2025-02-10 15:05:11 +0530

    Update Nav.jsx

--- Frontend/src/components/Nav.jsx
+++ Frontend/src/components/Nav.jsx
@@ -97,7 +97,7 @@ const Nav = () => {
           />
         {/* Mobile Menu */}
 {menuOpen && (
-  <div className="absolute top-full right-0 mt-2 w-64 bg-white shadow-lg rounded-lg p-4 z-50">
+<div className="absolute top-full right-0 mt-2 w-64 bg-white shadow-lg rounded-lg p-4 z-50">
     <ul className="flex flex-col gap-4">
       {(userInfo ? LoggedINnavLinks : navLinks).map((item) => (
         <li key={item.label}>
