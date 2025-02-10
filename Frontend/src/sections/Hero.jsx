b069a045b2e0f4c9f801301c02cadc0ec8a18c6a
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2025-02-10 15:47:16 +0530

    Update Hero.jsx

--- Frontend/src/sections/Hero.jsx
+++ Frontend/src/sections/Hero.jsx
@@ -11,10 +11,10 @@ const Hero = () => {
   return (
    <section
   id="home"
-  className="relative w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container pb-20 pt-16"
+  className="relative w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container pb-20 pt-15"
 >
 
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-20">
         <p className="text-xl font-montserrat text-coral-red pl-5">
           Our Summer collections
         </p>
