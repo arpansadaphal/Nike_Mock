bc718d8f9a2ce73a897311399365c277c0c393c3
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2025-02-11 16:13:10 +0530

    Update urls.py

--- Backend/nike/nike/urls.py
+++ Backend/nike/nike/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
 #if settings.DEBUG:
   #  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if not settings.DEBUG:
-    urlpatterns += [
-        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
+#if not settings.DEBUG:
+    #urlpatterns += [
+        #url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+  #  ]
