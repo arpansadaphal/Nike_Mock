d5edff087937e57029a8d20bb00b26bc122b7155
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2025-03-08 11:54:24 +0530

    Update ProductsPage.jsx

--- Frontend/src/components/Screens/ProductsPage.jsx
+++ Frontend/src/components/Screens/ProductsPage.jsx
@@ -99,7 +99,7 @@ const ProductsPage = () => {
           {/* {loading && <p>Loading...</p>} */}
           {error && <p>{error}</p>}
            {loading ? (
-            <div className="flex justify-center  w-full h-screen">
+            <div className="flex justify-center items-center w-full h-screen">
               <ClipLoader color="#F04F47" size={60} />
             </div>
           ) : ( 
